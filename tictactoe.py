d552beec04aa3239e13bb531735a0eb0c27694c4
fazevaib <vaibhavvats123@gmail.com>
2018-03-18 00:49:27 +0530

    Game Completed. Need Optimization.

--- tictactoe.py
+++ tictactoe.py
@@ -35,7 +35,80 @@ def ifWon():
 
 
 def GuessCompMove():
-    return random.randint(0, 8)
+    arr = [moveCondition_a(0, 1, 2), 
+    moveCondition_a(3, 4, 5),
+    moveCondition_a(6, 7, 8),
+    moveCondition_a(0, 3, 6),
+    moveCondition_a(1, 4, 7),
+    moveCondition_a(2, 5, 8),
+    moveCondition_a(2, 4, 6),
+    moveCondition_a(0, 4, 8),
+    moveCondition_b(0, 1, 2),
+    moveCondition_b(3, 4, 5),
+    moveCondition_b(6, 7, 8),
+    moveCondition_b(0, 3, 6),
+    moveCondition_b(1, 4, 7),
+    moveCondition_b(2, 5, 8),
+    moveCondition_b(2, 4, 6),
+    moveCondition_b(0, 4, 8)]
+
+    if arr[0] != -1:
+        return arr[0]
+    elif arr[1] != -1:
+        return arr[1]
+    elif arr[2] != -1:
+        return arr[2]
+    elif arr[3] != -1:
+        return arr[3]
+    elif arr[4] != -1:
+        return arr[4]
+    elif arr[5] != -1:
+        return arr[5]
+    elif arr[6] != -1:
+        return arr[6]
+    elif arr[7] != -1:
+        return arr[7]
+    elif arr[8] != -1:
+        return arr[8]
+    elif arr[9] != -1:
+        return arr[9]
+    elif arr[10] != -1:
+        return arr[10]
+    elif arr[11] != -1:
+        return arr[11]
+    elif arr[12] != -1:
+        return arr[12]
+    elif arr[13] != -1:
+        return arr[13]
+    elif arr[14] != -1:
+        return arr[14]
+    elif arr[15] != -1:
+        return arr[15]
+    else:
+        return random.randint(0, 8)
+
+
+def moveCondition_a(i, j, k):
+    if (boardValues[i] == boardValues[j] == compSym and boardValues[k] == ' '):
+        return k
+    elif (boardValues[i] == boardValues[k] == compSym and boardValues[j] == ' '):
+        return j
+    elif (boardValues[k] == boardValues[j] == compSym and boardValues[i] == ' '):
+        return i
+    else:
+        return -1
+
+def moveCondition_b(i, j, k):
+    if (boardValues[i] == boardValues[j] == playerSym and boardValues[k] == ' '):
+        return k
+    elif (boardValues[i] == boardValues[k] == playerSym and boardValues[j] == ' '):
+        return j
+    elif (boardValues[k] == boardValues[j] == playerSym and boardValues[i] == ' '):
+        return i
+    else:
+        return -1
+
+    
 
 def computerTurn():
     num = GuessCompMove()
@@ -45,7 +118,7 @@ def computerTurn():
     displayBoard()
 
     if ifWon():
-        print("You Lose")
+        print("YOU LOSE :(")
         quit()
         
 
@@ -60,7 +133,7 @@ def playerTurn():
     displayBoard()
 
     if ifWon():
-        print("You win")
+        print("YOU WON!")
         exit()
 
         
@@ -76,6 +149,11 @@ def nextTurn(turn):
         return 'Player'
     else:
         return 'Computer'
+
+def isDraw():
+    if ' ' not in boardValues:
+        print("MATCH DRAWN!")
+        exit()
 
 # def decideSymbol(firstWho):
 #     if firstWho == 'Computer':
@@ -95,4 +173,5 @@ if __name__ == '__main__':
         print('\n {} turn.\n'.format(currentTurn))
         makeYourTurn(currentTurn)
         currentTurn = nextTurn(currentTurn)
+        isDraw()
 
